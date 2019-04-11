f6af106cb7b837694e9328aa9702cf747f156756
Hilal <hilalarsa@gmail.com>
2019-04-12 00:31:36 +0700

    Add yup schema

--- src/helper/YupValidationSchema.js
+++ src/helper/YupValidationSchema.js
@@ -10,3 +10,27 @@ export const SignupSchema = Yup.object().shape({
     .max(50, 'Password is too long!')
     .required('Required'),
 });
+
+export const RegisterSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, 'Username is too short!')
+    .max(50, 'Username is too long!')
+    .required('Required'),
+  password: Yup.string()
+    .min(2, 'Password is too short!')
+    .max(50, 'Password is too long!')
+    .required('Required'),
+  confirmPassword: Yup.string()
+    .min(2, 'Password is too short!')
+    .max(50, 'Password is too long!')
+    // .matches(password, 'Password does not match')
+    .required('Required'),
+});
+
+export const EmailSubscriptionSchema = Yup.object().shape({
+  email: Yup.string()
+    .min(2, 'Username is too short!')
+    .max(50, 'Username is too long!')
+    .email('Email does not looks right')
+    .required('Required'),
+});
